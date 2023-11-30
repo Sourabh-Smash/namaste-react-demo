f9a0818bc24b47aaf2ac85fc85a08b77da0fe663
Sourabh Manawat <souramena123@gmail.com>
2023-11-30 21:22:18 +0530

    installing some packages

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
  
 const heading = React.createElement("h1", {}, "Sourabh");
+console.log(heading);
+
+const heading2 = <h1>Sourabh ji</h1>;
+console.log(heading2);
 
 const root = ReactDOM.createRoot(document.querySelector(".root"));
 root.render(heading);
+ 
