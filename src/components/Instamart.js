ac2f56b67743356b7ec784dff73faa5dd089adee
Sourabh Manawat <souramena123@gmail.com>
2023-12-27 22:32:41 +0530

    lifting the state up

--- src/components/Instamart.js
+++ src/components/Instamart.js
@@ -1,9 +1,60 @@
-import React from 'react'
-
-const Instamart = () => {
+import React, { useState } from "react";
+const Section = ({ title, description, isVisible, setIsVisible }) => {
+  console.log(setIsVisible);
   return (
-    <div>Instamart</div>
-  )
-}
+    <div className="border border-black p-2 m-2">
+      <h3 className="font-bold text-xl">{title}</h3>
+      {isVisible ? (
+        <button
+          onClick={() => setIsVisible(false)}
+          className="cursor-pointer underline"
+        >
+          Hide
+        </button>
+      ) : (
+        <button
+          onClick={() => setIsVisible(true)}
+          className="cursor-pointer underline"
+        >
+          Show
+        </button>
+      )}
+
+      {isVisible && <p>{description}</p>}
+    </div>
+  );
+};
+const Instamart = () => {
+  const [sectionConfig, setSectionConfig] = useState("team");
+  return (
+    <div>
+      <h1 className="text-3xl font-bold p-2 m-2 ">INSTAMART</h1>
+      <Section
+        isVisible={sectionConfig === "about"}
+        setIsVisible={() => setSectionConfig("about")}
+        title="About Instamart "
+        description={
+          "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."
+        }
+      />
+      <Section
+        isVisible={sectionConfig === "team"}
+        setIsVisible={() => setSectionConfig("team")}
+        title="Team Instamart "
+        description={
+          "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."
+        }
+      />
+      <Section
+        isVisible={sectionConfig === "careers"}
+        setIsVisible={() => setSectionConfig("careers")}
+        title="Careers"
+        description={
+          "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."
+        }
+      />
+    </div>
+  );
+};
 
 export default Instamart;
