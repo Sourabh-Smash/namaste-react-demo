91f629333b4b4656396c2ca7c075ee282b9b7255
Sourabh Manawat <souramena123@gmail.com>
2024-01-07 17:28:37 +0530

    made add to cart feature usign redux toolkit and refracting the code

--- src/utils/cartSlice.js
+++ src/utils/cartSlice.js
@@ -17,7 +17,6 @@ const cartSlice = createSlice({
     },
   },
 });
-console.log(cartSlice);
 export const { addItem, removeItem, clearCart } = cartSlice.actions;
 export default cartSlice.reducer;
  
